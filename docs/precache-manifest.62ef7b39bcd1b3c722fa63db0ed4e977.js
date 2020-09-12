c0a37e75dcfbd20cbc319ae1532ddc44772a630d
Fawad Ali <m.fawaadali98@gmail.com>
2020-09-12 15:40:20 +0500

    Redeploy

---
+++ docs/precache-manifest.62ef7b39bcd1b3c722fa63db0ed4e977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cbb61db206980958cbbe075e79cff0c",
+    "url": "/phoenix-react/index.html"
+  },
+  {
+    "revision": "5a79543ecd0e7ee137d0",
+    "url": "/phoenix-react/static/css/main.26908655.chunk.css"
+  },
+  {
+    "revision": "0dc2df9526d56ca8534d",
+    "url": "/phoenix-react/static/js/2.33b77187.chunk.js"
+  },
+  {
+    "revision": "e991a629d53db9276da8150ffc0bc99b",
+    "url": "/phoenix-react/static/js/2.33b77187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a79543ecd0e7ee137d0",
+    "url": "/phoenix-react/static/js/main.dce6f28d.chunk.js"
+  },
+  {
+    "revision": "8e975185670a42bb111553ad65eb8e5a",
+    "url": "/phoenix-react/static/js/main.dce6f28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3ea8e4853927286671",
+    "url": "/phoenix-react/static/js/runtime-main.ce2b2f5c.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e740b99106bf2c4f14f5365817f0892.js"
+  "/phoenix-react/precache-manifest.62ef7b39bcd1b3c722fa63db0ed4e977.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phoenix-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
